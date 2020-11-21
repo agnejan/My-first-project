6d8b87240dab06bae46f117dc1b4b8540c802d5a
agnejan <66556527+agnejan@users.noreply.github.com>
2020-11-21 15:00:58 +0200

    Added Axios

---
+++ weatherreact/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Weather from './Weather.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          < Weather />
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
